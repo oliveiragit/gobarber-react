78690018b716674d0b28bc0cf91c5e7647fb1a21
oliveiragit <deoliveirax@hotmail.com>
2020-08-10 13:09:01 -0300

    fix background full screem when scroll

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  height: 100%;
-  background: Linear-gradient(-90deg, #7159c1, #ab59c1);
   display: flex;
   justify-content: center;
   align-items: center;
+  min-height: 100%;
+  background: Linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
 
 export const Content = styled.div`
